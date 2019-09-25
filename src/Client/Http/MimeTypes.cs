838a20b66007dcd95387b04387f19f5ec88fcc06
rasmuseeg <rasmuseeg@outlook.com>
2019-09-25 20:23:26 +0200

    Updated license

--- src/Client/Http/MimeTypes.cs
+++ src/Client/Http/MimeTypes.cs
@@ -1,14 +1,14 @@
 ﻿namespace Doctrina.ExperienceApi.Client.Http
 {
-    internal static class MediaTypes
+    public static class MediaTypes
     {
-        internal static class Application
+        public static class Application
         {
             public const string Json = "application/json";
             public const string OctetStream = "application/octet-stream";
         }
 
-        internal static class Multipart
+        public static class Multipart
         {
             public const string Mixed = "multipart/mixed";
         }
