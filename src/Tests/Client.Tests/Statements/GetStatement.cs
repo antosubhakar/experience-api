bde82623a007c879f2fec2b7d014cd7577c6b0f1
rasmuseeg <rasmuseeg@outlook.com>
2019-10-06 12:05:38 +0200

    Added single get statement tests

--- src/Tests/Client.Tests/Statements/GetStatement.cs
+++ src/Tests/Client.Tests/Statements/GetStatement.cs
@@ -1,10 +1,10 @@
-using Doctrina.ExperienceApi.Client;
 using Doctrina.ExperienceApi.Client.Http.Headers;
 using Doctrina.ExperienceApi.Data;
 using RichardSzalay.MockHttp;
+using Shouldly;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,26 +12,88 @@ namespace Doctrina.ExperienceApi.Client.Tests.Statements
 {
     public class GetStatement
     {
+        public readonly BasicAuthHeaderValue AuthHeader = new BasicAuthHeaderValue("admin", "password");
+        public readonly Uri BaseAddress = new Uri("http://example.com/xAPI");
+
         [Fact]
-        public async Task Get_Statement_By_Id()
+        public async Task Get_Statement_By_Id_Format_Exact()
         {
             var mockHttp = new MockHttpMessageHandler();
+            var statement = new Statement("{'id':'c70c2b85-c294-464f-baca-cebd4fb9b348','timestamp':'2014-12-29T12:09:37.468Z','actor':{'objectType':'Agent','mbox':'mailto:example@example.com','name':'Test User'},'verb':{'id':'http://adlnet.gov/expapi/verbs/experienced','display':{'en-US':'experienced'}},'object':{'id':'http://example.com/xAPI/activities/myactivity','objectType':'Activity'}}");
 
-            // Setup a respond for the user api (including a wildcard in the URL)
-            mockHttp.When("http://example.com/xAPI/statements")
-                    // Respond with JSON
-                    .Respond("application/json", @"{'id':'c70c2b85-c294-464f-baca-cebd4fb9b348','timestamp':'2014-12-29T12:09:37.468Z','actor':{'objectType':'Agent','mbox':'mailto:example@example.com','name':'Test User'},'verb':{'id':'http://adlnet.gov/expapi/verbs/experienced','display':{'en-US':'experienced'}},'object':{'id':'http://example.com/xAPI/activities/myactivity','objectType':'Activity'}}"); 
+            var request = mockHttp.Expect(HttpMethod.Get, $"{BaseAddress}/statements")
+               .WithQueryString(new Dictionary<string, string>() {
+                    { "statementId", statement.Id.Value.ToString() },
+                    { "format", ResultFormat.Exact.ToString() }
+               })
+                   .Respond("application/json", statement.ToJson());
 
-            // Inject the handler or client into your application code
-            var httpClient = new HttpClient(mockHttp);
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = BaseAddress;
 
-            var authHeader = new BasicAuthHeaderValue("admin", "password");
-            var client = new LRSClient(authHeader, ApiVersion.GetLatest(), httpClient);
+            var client = new LRSClient(AuthHeader, ApiVersion.GetLatest(), httpClient);
 
-            var result = await client.GetStatement(Guid.Parse("c70c2b85-c294-464f-baca-cebd4fb9b348"));
+            var result = await client.GetStatement(statement.Id.Value);
 
-            // No network connection required
-            Console.Write(result.ToJson()); // {'name' : 'Test McGee'}
+            result.Id.Value.ShouldBe(statement.Id.Value);
+
+            mockHttp.GetMatchCount(request).ShouldBe(1);
+        }
+
+        [Fact]
+        public async Task Get_Statement_By_Id_Format_Cannonical()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            var statement = new Statement("{'id':'c70c2b85-c294-464f-baca-cebd4fb9b348','timestamp':'2014-12-29T12:09:37.468Z','actor':{'objectType':'Agent','mbox':'mailto:example@example.com','name':'Test User'},'verb':{'id':'http://adlnet.gov/expapi/verbs/experienced','display':{'en-US':'experienced'}},'object':{'id':'http://example.com/xAPI/activities/myactivity','objectType':'Activity'}}");
+
+            var request = mockHttp.Expect(HttpMethod.Get, $"{BaseAddress}/statements")
+                .WithQueryString(new Dictionary<string, string>() {
+                    { "statementId", statement.Id.Value.ToString() },
+                    { "format", ResultFormat.Cannonical.ToString() }
+                })
+                .Respond("application/json", statement.ToJson(ResultFormat.Cannonical));
+
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = BaseAddress;
+
+            var client = new LRSClient(AuthHeader, ApiVersion.GetLatest(), httpClient);
+
+            var result = await client.GetStatement(
+                statement.Id.Value, 
+                format: ResultFormat.Cannonical
+            );
+
+            result.Id.Value.ShouldBe(statement.Id.Value);
+
+            mockHttp.GetMatchCount(request).ShouldBe(1);
+        }
+
+        [Fact]
+        public async Task Get_Statement_By_Id_Format_Ids()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            var statement = new Statement("{'id':'c70c2b85-c294-464f-baca-cebd4fb9b348','timestamp':'2014-12-29T12:09:37.468Z','actor':{'objectType':'Agent','mbox':'mailto:example@example.com','name':'Test User'},'verb':{'id':'http://adlnet.gov/expapi/verbs/experienced','display':{'en-US':'experienced'}},'object':{'id':'http://example.com/xAPI/activities/myactivity','objectType':'Activity'}}");
+
+            var request = mockHttp.Expect(HttpMethod.Get, $"{BaseAddress}/statements")
+                .WithQueryString(new Dictionary<string, string>() {
+                    { "statementId", statement.Id.Value.ToString() },
+                    { "format", ResultFormat.Ids.ToString() }
+                })
+                .Respond("application/json", statement.ToJson(ResultFormat.Ids));
+
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = BaseAddress;
+
+            var client = new LRSClient(AuthHeader, ApiVersion.GetLatest(), httpClient);
+
+            var result = await client.GetStatement(
+                statement.Id.Value,
+                format: ResultFormat.Ids
+            );
+
+            result.Id.Value.ShouldBe(statement.Id.Value);
+
+            mockHttp.GetMatchCount(request).ShouldBe(1);
         }
     }
 }
